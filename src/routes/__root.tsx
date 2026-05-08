67900cb1488c181c0b9dbcede2b92ae399aa9cf8
Leonardo Salazar <leonardomeg123@gmail.com>
2026-05-08 10:31:48 -0300

    feat: add Vercel Analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import { Outlet, Link, createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
+import { Analytics } from "@vercel/analytics/react";
 
 import appCss from "../styles.css?url";
 
@@ -58,6 +59,7 @@ function RootShell({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <Analytics />
         <Scripts />
       </body>
     </html>
